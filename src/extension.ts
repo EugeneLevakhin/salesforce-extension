65782e98619a5684255c3da888e8652be102c088
Eugene Levakhin <eugene.levakhin@gmail.com>
2020-03-30 23:16:13 +0300

    extension with vue interaction

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -10,13 +11,20 @@ export function activate(context: vscode.ExtensionContext) {
 
 	disposable = vscode.commands.registerCommand('extension.showView', () => {
 		const panel = vscode.window.createWebviewPanel(
-			'catCoding',
-			'Cat Coding',
+			'webView',
+			'Web view',
 			vscode.ViewColumn.One,
-			{}
+			{
+				enableScripts: true
+			}
 		);
 
-		panel.webview.html = getWebviewContent();
+		const onDiskPath = vscode.Uri.file(
+			path.join(context.extensionPath, 'presentation', 'dist')
+		);
+
+		const imgUrl = panel.webview.asWebviewUri(onDiskPath);
+		panel.webview.html = getWebviewContent(imgUrl.toString());
 	});
 
 	context.subscriptions.push(disposable);
@@ -25,16 +33,28 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {
 }
 
-function getWebviewContent() {
+function getWebviewContent(path: string) {
 	return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-	  <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <title>Cat Coding</title>
-  </head>
-  <body>
-	  <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
-  </body>
-  </html>`;
+	<html lang=en>
+	
+	<head>
+		<meta charset=utf-8>
+		<meta http-equiv=X-UA-Compatible content="IE=edge">
+		<meta name=viewport content="width=device-width,initial-scale=1">
+		<link rel=icon href=/favicon.ico>
+		<title>presentation</title>
+		<link href=${path}/css/app.css rel=preload as=style>
+		<link href=${path}/js/app.js rel=preload as=script>
+		<link href=${path}/js/chunk-vendors.js rel=preload as=script>
+		<link href=${path}/css/app.css rel=stylesheet>
+	</head>
+	
+	<body><noscript><strong>We're sorry but presentation doesn't work properly without JavaScript enabled. Please enable it
+				to continue.</strong></noscript>
+		<div id=app></div>
+		<script src=${path}/js/chunk-vendors.js></script>
+		<script src=${path}/js/app.js></script>
+	</body>
+	
+	</html>`;
 }
